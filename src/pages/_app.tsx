c646b41c489b56e0a616c0cba332008c62eefa02
novellito <novellito7@gmail.com>
2022-07-07 21:18:17 -0700

    add styled components & sass

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,13 @@
-import withReduxStore from '../lib/with-redux-store';
-import { Provider } from 'react-redux';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import { AppProps } from 'next/app';
-import { createGlobalStyle } from 'styled-components';
+import type { AppProps } from 'next/app';
 
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
-const GlobalStyle = createGlobalStyle`
- * {
-    box-sizing: border-box;
-  }
-  html {
-    height: 100%;
-  }
-`;
+import '../styles/globals.scss';
 
-interface AppPropsWithRedux extends AppProps {
-  reduxStore: any;
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
-function MyApp({ Component, pageProps, reduxStore }: AppPropsWithRedux) {
-  return (
-    <Provider store={reduxStore}>
-      <Component {...pageProps} />
-      <GlobalStyle />
-    </Provider>
-  );
-}
-
-export default withReduxStore(MyApp);
+export default MyApp;
